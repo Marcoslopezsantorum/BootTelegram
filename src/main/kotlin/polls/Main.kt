82768cb5da784342bf3921843bd92ecc58ba7e0d
Marcoslopezsantorum <72596886+Marcoslopezsantorum@users.noreply.github.com>
2021-05-17 11:57:00 +0200

    Marcos (#3)
    * añado el polls + rama
    
    * añado el webhook + explico el polls
    
    * añado  comentarios y soluciono problemas

--- src/main/kotlin/polls/Main.kt
+++ src/main/kotlin/polls/Main.kt
@@ -1,5 +1,6 @@
 package polls
 
+
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.command
@@ -7,9 +8,6 @@ import com.github.kotlintelegrambot.dispatcher.pollAnswer
 import com.github.kotlintelegrambot.entities.ChatId
 import com.github.kotlintelegrambot.entities.polls.PollType.QUIZ
 
-fun main() {
-    bot {
-        token = "BOT_API_TOKEN"
         dispatch {
 
 //Devuelve un mensaje con la opcion que ha escogido
@@ -18,8 +16,7 @@ fun main() {
                 println("${pollAnswer.user.username} has selected the option ${pollAnswer.optionIds.lastOrNull()} in the poll ${pollAnswer.pollId}")
             }
 
-//Comando que mostrara una encuesta con los equipos de futbol
-            command("quizPoll") {
+
                 bot.sendPoll(
 //Recoge la id del usuario que ha votado
                     chatId = ChatId.fromId(message.chat.id),
@@ -38,25 +35,12 @@ fun main() {
                 )
             }
 
-            command("quizPoll") {
-                bot.sendPoll(
-                    chatId = ChatId.fromId(message.chat.id),
-                    type = QUIZ,
-                    question = "Mejor defensa del mundo?",
-                    options = listOf("Piqué", "Ramos", "Puyol"),
-                    correctOptionId = 3,
+
                     isAnonymous = false
                 )
             }
 
-            command("closedPoll") {
-                bot.sendPoll(
-                    chatId = ChatId.fromId(message.chat.id),
-                    type = QUIZ,
-                    question = "Cual es el mejor jugador del mundo",
-                    options = listOf("Neymar", "Messi", "CR7"),
-                    correctOptionId = 2,
-                    isClosed = true,
+
                     explanation = "Porque tiene mas balones de oro"
                 )
             }

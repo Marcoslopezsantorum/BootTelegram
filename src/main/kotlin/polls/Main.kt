edd256a535ed060a3d72a4384940c381c7f99454
Marcoslopezsantorum <mlopezsantorum@danielcastelao.org>
2021-05-17 11:24:05 +0200

    añado el webhook + explico el polls

--- src/main/kotlin/polls/Main.kt
+++ src/main/kotlin/polls/Main.kt
@@ -11,15 +11,30 @@ fun main() {
     bot {
         token = "BOT_API_TOKEN"
         dispatch {
+
+//Devuelve un mensaje con la opcion que ha escogido
+
             pollAnswer {
                 println("${pollAnswer.user.username} has selected the option ${pollAnswer.optionIds.lastOrNull()} in the poll ${pollAnswer.pollId}")
             }
-            command("regularPoll") {
+
+//Comando que mostrara una encuesta con los equipos de futbol
+            command("quizPoll") {
                 bot.sendPoll(
+//Recoge la id del usuario que ha votado
                     chatId = ChatId.fromId(message.chat.id),
-                    question = "Pizza with or without pineapple?",
-                    options = listOf("With :(", "Without :)"),
-                    isAnonymous = false
+//Tipo de encuesta
+                    type = QUIZ,
+//Pregunta
+                    question = "Cual es el mejor equipo de futbol",
+//Todas las opciones
+                    options = listOf("Barsa", "Madrid", "Celta","Atletico de Madrid"),
+//Cual es la opcion correcta
+                    correctOptionId = 1,
+//Si la votacion es anónima o no
+                    isAnonymous = false,
+//El tiempo que dura la votacion
+                    openPeriod = 120,
                 )
             }
 
@@ -27,9 +42,9 @@ fun main() {
                 bot.sendPoll(
                     chatId = ChatId.fromId(message.chat.id),
                     type = QUIZ,
-                    question = "Java or Kotlin?",
-                    options = listOf("Java", "Kotlin"),
-                    correctOptionId = 1,
+                    question = "Mejor defensa del mundo?",
+                    options = listOf("Piqué", "Ramos", "Puyol"),
+                    correctOptionId = 3,
                     isAnonymous = false
                 )
             }
@@ -38,11 +53,11 @@ fun main() {
                 bot.sendPoll(
                     chatId = ChatId.fromId(message.chat.id),
                     type = QUIZ,
-                    question = "Foo or Bar?",
-                    options = listOf("Foo", "Bar", "FooBar"),
-                    correctOptionId = 1,
+                    question = "Cual es el mejor jugador del mundo",
+                    options = listOf("Neymar", "Messi", "CR7"),
+                    correctOptionId = 2,
                     isClosed = true,
-                    explanation = "A closed quiz because I can"
+                    explanation = "Porque tiene mas balones de oro"
                 )
             }
         }

b056b60b72af659f8004d22836295c38d5bc0d61
daaviidm10 <72596994+daaviidm10@users.noreply.github.com>
2021-05-17 16:52:16 +0200

    Revert "Rama david (#7)"
    This reverts commit add2338eac1beccc8cff027c7c9b27f9126af1ba.

--- src/main/kotlin/dispatcher/dispatcher.kt
+++ src/main/kotlin/dispatcher/dispatcher.kt
@@ -2,7 +2,23 @@ package dispatcher
 
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.text
+import com.github.kotlintelegrambot.entities.ChatId
+import com.github.kotlintelegrambot.network.fold
 
+fun main() {
+    val bot = bot {
+        token = Apikey.miToken
+        dispatch {
+            text {
+                print("Chat ID: ")
+                println(message.chat.id)
+                bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = text)
+            }
+            command("start") {
+
+                val result = bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "El bot se ha iniciado")
 
                 result.fold({
 
@@ -10,4 +26,7 @@ import com.github.kotlintelegrambot.dispatch
                     // do something with the error
                 })
             }
-
+        }
+    }
+    bot.startPolling()
+}
